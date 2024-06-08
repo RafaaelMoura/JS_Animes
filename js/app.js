67cc22d328379a5b8e859958d671547d81aa189d
rafamour03@gmail.com <rafamour03@gmail.com>
2024-06-08 17:26:21 -0300

    operadores matematicos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,47 @@
 console.log ('Treinameto com JS')
 
-var userName = 'Otaku'
+//Variáveis////////
 
+var userName = 'Otaku'
 document.getElementById('user-name').innerHTML = userName
 
 
-// var nome = 'JavaScript Animes'
-// var idade = 1
-// var animes = true
+ var nome = 'JavaScript Animes'
+ var idade = 1
+ var animes = true
 
-// console.log(typeof nome)
-// console.log(typeof idade)
-// console.log(typeof true)
+ console.log(typeof nome)
+ console.log(typeof idade)
+ console.log(typeof true)
+
+// Operadores Matematicos 
+
+ var n1 = 5
+ var n2 = 5
+
+ var total = n1 + n2
+ console.log(total)
+
+// - Tipo Texto 
+
+ var n1 = 5
+ var n2 = 'cinco'
+
+ var total = n1 + parseInt(n2)
+ console.log(total)
+
+ // - números com pontos
+
+ var n1 = 5
+ var n2 = 2.5
+
+ console.log(typeof n1)
+ console.log(typeof n2)
+
+ var total = n1/n2
+ console.log(total)
+
+
+
+
 
