715d73b00d598a520ea5caa8bd5003de3cb7d776
rafamour03@gmail.com <rafamour03@gmail.com>
2024-06-08 19:47:34 -0300

    Aplicação Vues

---
+++ js/basic.js
@@ -0,0 +1,77 @@
+
+console.log ('Treinameto com JS')
+
+//Variáveis////////
+
+var userName = 'Otaku'
+document.getElementById('user-name').innerHTML = userName
+
+
+ var nome = 'JavaScript Animes'
+ var idade = 1
+ var animes = true
+
+ console.log(typeof nome)
+ console.log(typeof idade)
+ console.log(typeof true)
+
+//Operadores Matematicos 
+
+ var n1 = 5
+ var n2 = 5
+
+ var total = n1 + n2
+ console.log(total)
+
+//Tipo Texto 
+
+ var n1 = 5
+ var n2 = 'cinco'
+
+ var total = n1 + parseInt(n2)
+ console.log(total)
+
+ //números com pontos
+
+ var n1 = 5
+ var n2 = 2.5
+
+ console.log(typeof n1)
+ console.log(typeof n2)
+
+ var total = n1/n2
+ console.log(total)
+
+ //Operadores de Comparação
+
+ var v1 = 5
+ var v2 = '5'
+
+ var resultado = v1 !== v2
+ console.log(resultado)
+
+ //Funções
+
+ function soma (n1, n2) {
+    console.log(n1 + n2)
+ }
+  soma(5,100)
+
+/// Boas vindas
+
+function boasvindas(nome){
+    alert (nome +', seja bem Vindo(a)')
+}
+
+boasvindas('Otaku')
+
+// Retorno
+
+function soma(n1, n2) {
+    return n1 + n2
+ }
+  var resultado = soma(5,5)
+  console.log(resultado)
+
+/////aula 14 à 18/////
+
